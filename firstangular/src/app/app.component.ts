0abedcf53f86a5e3be6c0529c96ce7b011108083
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-10-25 15:54:25 -0300

    Two way data binding module

--- firstangular/src/app/app.component.ts
+++ firstangular/src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['app.component.scss'],
+  templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
-  title = 'firstangular';
+  title: string 
+  isHappy: boolean = false
+  numberOne: number = 1
+  numberTwo: number = 2
+  name: string = 'Vini'
+
+  constructor(){
+    this.title = 'Ultimate Angular'
+  }
+
+  handleClick(){
+    this.name = 'Vini do Botão'
+  }
+
+  handleChange(value: any){ 
+    this.name = value
+  }
 }
